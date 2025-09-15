c8b8bb7e14060406eef472c779f33525ad8c0a2c
ali-morgan <alimorgan1527@gmail.com>
2025-09-15 18:47:40 +0300

    Added player movement, and randomly sized asteroids that move around

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 import constants
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -10,15 +13,37 @@ def main():
     if failed_mod_inits > 0:
         print(f"Failed to initialize {failed_mod_inits} modules.")
 
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    asteroid_group = pygame.sprite.Group()
+
+    AsteroidField.containers = updatable_group
+    Asteroid.containers = (asteroid_group, updatable_group, drawable_group)
+    field = AsteroidField()
+
+    Player.containers = (updatable_group, drawable_group)
+    p = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     window = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    deltaTime = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # p.update(deltaTime)
+        updatable_group.update(deltaTime)
+
         window.fill(color="black")
+        # p.draw(window)
+        for drawable in drawable_group:
+            drawable.draw(window)
         pygame.display.flip()
+
+        dtMillis = clock.tick(constants.TARGET_FPS)
+        deltaTime = dtMillis / 1000
 
 
 
